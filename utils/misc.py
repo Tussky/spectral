707e820fa4ec843cb633adae0e487115eeeee6c1
tusky <isaac@dw-perspective.org.uk>
2025-12-03 19:50:36 -0500

    Feature(reading in files with h5py)

---
+++ utils/misc.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np 
+import h5py
+
+def read_in_hdf5_spectra(path = "./../Data/Gamma/210601_NBS295-106/20210601_152616_mass-001.hdf5"):
+    """
+    Args:
+        path (str): The path to your file. Defaults.
+    Returns: 
+        pd.DataFrame: A DataFrame of series for all included channels.
+    
+    """
+    filename = "./../Data/Gamma/210601_NBS295-106/20210601_152616_mass-001.hdf5"
+    with h5py.File(filename, "r") as hdf_file:
+        channels = pd.DataFrame(
+            columns=["energy"],
+            index=hdf_file.keys(),
+        )
+
+        for channel_name in hdf_file:
+            going_in = np.array(hdf_file[channel_name]["filt_value"])
+            going_in = going_in[(0 < going_in) & (going_in < np.percentile(going_in, 97))]
+            channels.loc[channel_name] = [going_in]
